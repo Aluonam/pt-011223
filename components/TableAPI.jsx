c032d2f94090910e3355c4c5ca5fc1abdcb81dfc
Aluon <paulacanoroman@gmail.com>
2023-12-02 00:01:00 +0100

    feat: api call with useEffect

--- components/TableAPI.jsx
+++ components/TableAPI.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TableAPI = () => {
+
+    const [dataAPI, setDataAPI] = useState([])
+
+    useEffect(() => {
+      const apiCall = async ()=>{
+        try{
+            const url = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+            const data = await url.json();
+            console.log(data)
+        } catch (error){error, "error detected"}
+      }
+      apiCall()
+    }, [])
+    
+
+
   return (
     <div>TableAPI</div>
   )
